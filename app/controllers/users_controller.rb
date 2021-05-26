3f110f448fc6161f094d276e43e87010e4e5ac19
Jon Chen <jonathan.chen.6799@gmail.com>
2021-05-26 16:07:37 -0500

    Finished controller exercise

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,43 @@
 class UsersController < ApplicationController
 
   def index
-    render plain: "I'm in the index action!"
+    render json: User.all
   end
 
   def create
-    # render plain: "I'm in the index action!"
-    render json: params
+    @user = User.new(user_params)
+  # replace the `user_attributes_here` with the actual attribute keys
+    if @user.save!
+      render json: @user
+    else
+      render json: @user.errors.full_messages, status: 422
+    end
+
   end
 
   def show
-    render json: params
+    @user = User.find(params[:id])
+    render json: @user
+  end
+
+  def destroy
+    user = User.find(params[:id])
+    user.destroy
+    render json: user
+    # self.index
+  end
+
+  def update
+    @user = User.find(params[:id])
+
+    if @user.update!(user_params)
+      render json: @user
+    else
+      render json: @user.errors.full_messages, status: :unprocessable_entity
+    end
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :email)
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+ActiveRecord::Base.transaction do
+
+potato = User.create(id: 1, name: 'Potato', email: 'Potato@gmail.com')
+apple = User.create(id: 2, name: 'Apple', email: 'Apple@gmail.com')
+mango = User.create(id: 3, name: 'Mango', email: 'Mango@gmail.com')
+
+
+end
